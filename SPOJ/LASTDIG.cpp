58a9f21837b1b707277a13b42b70733a1a6adab4
babybrain <manav.sonejaav@gmail.com>
2022-05-03 17:29:45 +0530

    New Problems Solved

--- SPOJ/LASTDIG.cpp
+++ SPOJ/LASTDIG.cpp
@@ -1,41 +1,23 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-#define ll long long
-int main(){
-vector<int> v(10,0);
-v[0]=0;
-v[1]=1;
-v[2]=4;
-v[3]=4;
-v[4]=2;
-v[5]=1;
-v[6]=1;
-v[7]=4;
-v[8]=4;
-v[9]=2;
-int T;
-cin>>T;
-while(T--){
-    ll a,b;
-    cin>>a>>b;
-    if (b!=0){
-    ll val=a%10;
-    ll cycle=v[val];
-    if(cycle==0){
-        cout<<0<<endl;
-    }
-    else{
-    ll rem=b%cycle;
-    if(rem==0)
-        rem=cycle;
-    val=pow(val,rem);
-    cout<<val%10<<endl;
-    }
-    }
-
-    else{
-        cout<<1<<endl;
-    }
+int solve(int a,int b){
+    if(b==0)
+        return 1;
+    else if(a==0)
+        return 0;
+    int res=solve(a,b/2)%10;
+    if(b%2)
+        return ((a%10)*(res)*(res))%10;
+    else
+        return (res*res)%10;
 
 }
+int main(){
+    int T;
+    cin>>T;
+    while(T--){
+        int a,b;
+        cin>>a>>b;
+        cout<<solve(a,b)<<endl;
+    }
 }
--- SPOJ/LOCKER.cpp
+++ SPOJ/LOCKER.cpp
@@ -1,27 +1,41 @@
-#include<bits/stdc++.h>
+#include<iostream>
 using namespace std;
-#define ll long long
-#define MOD 1000000007
 
+const long long mod=1000000007;
 
+long long solve(long long base,long long exp){
+    if(exp==0)
+        return 1;
+    if(exp==1)
+        return base%mod;
+    long long res=solve(base,exp/2);
+    if(exp%2)
+        return ((base%mod)*(res%mod)*(res%mod))%mod;
+    else
+        return ((res%mod)*(res%mod))%mod;
 
+}
 int main(){
     int T;
     cin>>T;
     while(T--){
-        ll n,ans;
+        long long n,n1;
         cin>>n;
-        if(n%2==0){
-            ans=(((n/2)%MOD)*((n/2)%MOD))%MOD;
-        }
+        if(n>1)
+        {
+        if(n%3==0)
+            cout<<solve(3,n/3)%mod<<endl;
+        else if(n%3==1)
+            {
+                n1=(n-4)/3;
+                cout<<(4*solve(3,n1))%mod<<endl;
+            }
         else{
-            ans=(((n/2)%MOD)*(((n/2)+1)%MOD))%MOD;
+            n1=(n-2)/3;
+            cout<<(2*solve(3,n1))%mod<<endl;
         }
-        if(n==1||n==2){
-            cout<<n<<endl;
         }
-        else{
-            cout<<ans%MOD<<endl;
-        }
+        else
+            cout<<1<<endl;
     }
 }
